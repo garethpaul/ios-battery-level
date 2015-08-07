eadc70eb5d827cca3b64e15e050b6aefafed63b5
Gareth Jones <gareth@garethpaul.com>
2015-08-06 20:00:00 -0600

    adding swift changes:

--- ChargeMe/ViewController.swift
+++ ChargeMe/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Get Device Battery Level
         var x = UIDevice.currentDevice().batteryLevel
         
         
